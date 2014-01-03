953acf100c52f560d06f6d8e4974ae208d574274
hery <aeonzamaek@gmail.com>
2014-01-03 17:09:44 -0500

    Initial Commit

---
+++ ErenTests/ErenTests.m
@@ -0,0 +1,34 @@
+//
+//  ErenTests.m
+//  ErenTests
+//
+//  Created by Hery on 1/3/14.
+//  Copyright (c) 2014 Hery Ratsimihah. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ErenTests : XCTestCase
+
+@end
+
+@implementation ErenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
